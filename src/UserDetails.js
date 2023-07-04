373957d9f345b9530b4c38d4b08d92f7ffde4330
deepika joshi <upreti.deepika@gmail.com>
2023-07-04 23:40:44 +0800

    added delete and update button with functionality

--- src/UserDetails.js
+++ src/UserDetails.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { useLoaderData } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 export async function loader({ params }) {
   const API = `http://localhost:3000/api/user/${params.userid}`;
+
 
   try {
     const response = await fetch(API);
@@ -21,18 +24,90 @@ export async function loader({ params }) {
 
 export default function UserDetail() {
   const data = useLoaderData();
+  const navigate = useNavigate();
+
+  // first show user a form to update the current user details
+  const [name, setName] = useState(data.user.name);
+  const [email, setEmail] = useState(data.user.email);
+  const [address, setAddress] = useState(data.user.address);
+  // once he updates the data, then call this method to update
+  const handleUpdate = async () => {
+    try {
+      // Send a PUT request to update the user's data
+      const response = await axios.put(
+        `http://localhost:3000/api/user/${data.user._id}`,
+        { name, email, address }
+      );
+
+      // Handle the response as needed
+      alert('User data updated successfully:', response.data);
+      navigate(`/`);
+    } catch (err) {
+      console.err('Error updating user data:', err);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      // Send a DELETE request to delete the user's data
+      const response = await axios.delete(
+        `http://localhost:3000/api/user/${data.user._id}`
+      );
+
+      // Handle the response as needed
+      alert('User data deleted successfully:', response.data);
+      navigate(`/`);
+    } catch (err) {
+      console.err('Error deleting user data:', err);
+    }
+  };
 
   return (
     <div>
       <div className="card">
         <div className="card-body">
           <h5 className="card-title">User Details</h5>
-          Name : <p className="card-text"> {data.user.name}</p>
-          Email:<p className="card-text"> {data.user.email}</p>
-          Address :<p className="card-text"> {data.user.address}</p>
+          <form onSubmit={handleUpdate}>
+            <div className="form-group">
+              <label htmlFor="nameInput">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="nameInput"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="emailInput">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="emailInput"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="addressInput">Address</label>
+              <input
+                type="text"
+                className="form-control"
+                id="addressInput"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
+            </div>
+
+              <button type="submit" className="btn btn-primary">
+                Update
+              </button>
+          </form>
+          <button className="btn btn-danger" onClick={handleDelete}>
+            Delete
+          </button>
         </div>
       </div>
     </div>
-
   );
 }
