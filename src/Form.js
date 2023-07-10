3534ce6a0ec867f54a1e03daee56af718aab644c
deepika joshi <upreti.deepika@gmail.com>
2023-07-10 12:57:10 +0800

    added form with new fields

--- src/Form.js
+++ src/Form.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-
 const Form = () => {
-  const [name, setName] = useState('');
+  const [salutation, setSalutation] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [gender, setGender] = useState('');
   const [email, setEmail] = useState('');
-  const [address, setAddress] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     let response;
     try {
       response = await axios.post('http://localhost:3000/api/user', {
-        name,
+        salutation,
+        firstName,
+        lastName,
+        dateOfBirth,
+        gender,
         email,
-        address,
       });
       // Handle successful response
       console.log('Form submitted successfully:', response);
@@ -23,51 +28,123 @@ const Form = () => {
       console.error('Error submitting form:', error);
       alert('Error submitting form. Please try again.');
     }
-    if (response.status === 201) {
-      alert("user is saved !")
-      // Clear textarea
-      setName('');
+    if (response && response.status === 201) {
+      alert('User is saved!');
+      // Clear fields
+      setSalutation('');
+      setFirstName('');
+      setLastName('');
+      setDateOfBirth('');
+      setGender('');
       setEmail('');
-      setAddress('');
-
     }
-
-
   };
-
 
   return (
     <div className="container">
       <div className="row">
         <div className="mx-auto col-10 col-md-8 col-lg-6">
-          <form onSubmit={handleSubmit} >
+          <form onSubmit={handleSubmit}>
             <div className="form-group row">
-              <label htmlFor="name">Name</label>
+              <label htmlFor="salutation">Salutation</label>
               <div className="col-sm-10">
-                <input type="name" className="form-control" id="name" placeholder="Name" value={name}
-                  onChange={(e) => setName(e.target.value)} />
+                <select
+                  className="form-control"
+                  id="salutation"
+                  value={salutation}
+                  onChange={(e) => setSalutation(e.target.value)}
+                  required
+                >
+                  <option value="">Select Salutation</option>
+                  <option value="Ms">Ms</option>
+                  <option value="Mrs">Mrs</option>
+                  <option value="Madam">Madam</option>
+                  <option value="Mr">Mr</option>
+                  <option value="Master">Master</option>
+                  <option value="Sir">Sir</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label htmlFor="firstName">First Name</label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="firstName"
+                  placeholder="First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label htmlFor="lastName">Last Name</label>
+              <div className="col-sm-10">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lastName"
+                  placeholder="Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label htmlFor="dateOfBirth">Date of Birth</label>
+              <div className="col-sm-10">
+                <input
+                  type="date"
+                  className="form-control"
+                  id="dateOfBirth"
+                  value={dateOfBirth}
+                  onChange={(e) => setDateOfBirth(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="form-group row">
+              <label htmlFor="gender">Gender</label>
+              <div className="col-sm-10">
+                <select
+                  className="form-control"
+                  id="gender"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  required
+                >
+                  <option value="">Select Gender</option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+                </select>
               </div>
             </div>
 
             <div className="form-group row">
               <label htmlFor="email">Email</label>
               <div className="col-sm-10">
-                <input type="email" className="form-control" id="email" placeholder="Email"
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Email"
                   value={email}
-                  onChange={(e) => setEmail(e.target.value)} />
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </div>
             </div>
 
-
-            <div className="form-group row">
-              <label htmlFor="address">Address</label>
-              <div className="col-sm-10">
-                <input type="address" className="form-control" id="address"
-                  placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)} />
-              </div>
-            </div>
-
-            <button type="submit" className="btn btn-primary mb-2">Submit</button>
+            <button type="submit" className="btn btn-primary mb-2">
+              Submit
+            </button>
           </form>
         </div>
       </div>
